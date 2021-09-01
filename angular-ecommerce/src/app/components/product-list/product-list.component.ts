32173301c2f687e995239feaecfad0ef85dfc9ae
meait-3 <tchikh3@@gmail.com>
2021-09-02 00:25:25 +0100

    html to pdf

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
-
+import html2canvas from 'html2canvas';
+import * as jspdf from 'jspdf';
+import { jsPDF } from "jspdf";
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-table.component.html',
@@ -9,7 +11,19 @@ import { Product } from 'src/app/common/product';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
+ 
+  generatePDF() {
+    var data = document.getElementById('contentToConvert');
+    html2canvas(data).then(canvas => {
+      var imgWidth = 208;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      const contentDataURL = canvas.toDataURL('image/png')
+      let pdf = new jsPDF('p', 'mm', 'a4');
+      var position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      pdf.save('newPDF.pdf');
+    });
+  }
   products: Product[];
   
   constructor(private productService: ProductService) { }
